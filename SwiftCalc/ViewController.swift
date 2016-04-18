33e0ebfb44e5422fc085f9ad0bf59620567d780b
Rene Haavre <reneha@microsoft.com>
2016-04-18 16:03:44 +0300

    Initial Commit

---
+++ SwiftCalc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftCalc
+//
+//  Created by Rene Haavre on 18/04/16.
+//  Copyright © 2016 Rene Haavre. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
